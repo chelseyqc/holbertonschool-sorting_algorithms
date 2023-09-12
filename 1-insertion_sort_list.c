ac2fbd2be8f7ccc30699d54932b2fd21cdd0274e
root <6721@holbertonstudents.com>
2023-09-12 00:29:21 -0700

    fix: fixed the printing

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -16,61 +16,50 @@ void insert_sorted(listint_t **list, listint_t *node1, listint_t *node2)
 	if (node1->prev != NULL)
 	{
 		node1->prev->next = node2;
+		node2->prev = node1->prev;
 	}
 	else
 	{
 		*list = node2;
+		node2->prev = NULL;
 	}
 	if (node2->next != NULL)
 	{
 		node2->next->prev = node1;
+		node1->next = node2->next;
 	}
-
-	node1->next = node2->next;
-	node2->prev = node1->prev;
+	else
+	{
+		node1->next = NULL;
+	}
 	node1->prev = node2;
 	node2->next = node1;
 }
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *key;
-	listint_t *current;
-	listint_t *prev;
+	listint_t *node1;
+	listint_t *node2;
 
 	if (list == NULL || *list == NULL)
 	{
 		return;
 	}
 
-	key = (*list)->next;
+	node1 = *list;
 
-	while (key != NULL)
+	while (node1->next != NULL)
 	{
-		current = key;
-		prev = key->prev;
-
-		while (prev != NULL && prev->n > current->n)
+		node2 = node1->next;
+		if (node1->n > node2->n)
 		{
-			prev->next = current->next;
-			if (current->next != NULL)
-			{
-				current->next->prev = prev;
-			}
-			current->prev = prev->prev;
-			current->next = prev;
-			if (prev->prev != NULL)
-			{
-				prev->prev->next = current;
-			}
-			else
-			{
-				*list = current;
-			}
-			prev->prev = current;
-			prev = current->prev;
+			insert_sorted(list, node1, node2);
+			print_list(*list);
+			node1 = *list;
 		}
-		key = key->next;
-		print_list(*list);
+		else
+		{
+			node1 = node1->next;
+		}
 	}
 }
